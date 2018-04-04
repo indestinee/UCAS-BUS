1f9fb492d03c78fe8c33005a18d73270f1fbfc4c
indestinee Chen <indestinee@gmail.com>
2018-04-05 00:17:39 +0800

    baseline

---
+++ README.md
@@ -0,0 +1,28 @@
+#semi-automatic bus tickets of UCAS ordering
+This scripts are only coded for sharing knowledge 'how to write a spider/crawl' to a real website. All codes are only for learning.
+##running screen shots
+
+##prepare
+```
+1. Python3 environment in your computer. 
+2. Make a soft link from crawl2 to crawl. 
+```
+##WARNING
+When/After you run, there will be some cache files and user information in files, like \*cache and data.  
+**'NEVER GIVE ANYONE ANY OF THOSE CACHE FILES!!!'**
+##requirement
+```
+1. python3 and evironment
+2. opencv in python3 (it does not matter if you dont have. but you have to open picture of cert code on your own.)
+```
+##how to run
+```
+1. python3 main.py # or make 
+2. input everything it requires, when it shows '[I N]'.
+3. after the 5-th step, you will receive a long url $URL.
+4. login in your browser first, then open $URL in step 3.
+5. you can see you have one order in buying status.
+6. scan the QR code. and pay with Wechat. and some minutes later, the status will turn to done.
+```
+##knowledge of crawl/spider
+google/baidu it or read some <a href='http://docs.python-requests.org/zh_CN/latest/user/quickstart.html'>python-request docs</a>
---
+++ main.py
@@ -0,0 +1,112 @@
+from crawl.spider import *
+from crawl.utils import *
+from IPython import embed
+
+cache = Cache('./cache')
+html_path = 'html_cache'
+touchdir(html_path)
+
+spider = Spider()
+
+def success_judge(response):
+    response.encoding = 'utf-8'
+    return '你好' in response.text
+
+
+
+while not spider.login('http://payment.ucas.ac.cn/NetWorkUI/fontuserLogin', \
+        success_judge, 'http://payment.ucas.ac.cn/NetWorkUI/authImage'):
+    print('[ERR] failed to login please check username, password and certcode')
+
+spider.download('bus.html', 'http://payment.ucas.ac.cn/NetWorkUI/reservedBus514R001')
+
+def first_step():# {{{ 
+    #   first step is useless for advanced user, if you didn't know which routecode, 
+    #   you can see elements from website
+    data = {
+        'carno': '0020',
+        'payProjectId': '4',
+        'factorycode': 'R001',
+        'payamtstr': '6.00',
+        'freeseat': '37',
+        'routecode': '0020',
+        'bookingdate': '2018-04-06',
+        'routeName': '雁栖湖—玉泉路15:40（节假日）',
+        'payAmt': '6.00',
+    }
+    url = 'http://payment.ucas.ac.cn/NetWorkUI/openReservedBusInfoConfirm'
+    print('[LOG] posting to', url)
+    response = spider.post(url, data)
+    spider.html_save(response, 'first_step.html')
+    return response
+# }}}
+def second_step():# {{{
+    phone_cache = Cache('./data')
+    tel = phone_cache.load('tel')
+    if not tel:
+        tel = input('[I N] phone number: ')
+        phone_cache.save(tel, 'tel')
+    data = {
+        'routecode': '0020',            #   You need change
+        'payAmt': '6.00',
+        'bookingdate': '2018-04-06',    #   You need change
+        'payProjectId': '4',        
+        'tel': tel,
+        'factorycode': 'R001',
+    }
+    url = 'http://payment.ucas.ac.cn/NetWorkUI/reservedBusCreateOrder'
+    print('[LOG] step 2, posting to', url)
+    response = spider.post(url, data)
+    spider.html_save(response, 'second_step.html')
+    return response
+# }}}
+def third_step():# {{{
+    data = {
+        'orderno': information['payOrderTrade']['orderno'],
+        'orderamt': '6.00',
+        'payType': '03',
+        'mess': '',
+        'start_limittxtime': '',
+        'end_limittxtime': '',
+    }
+    url = 'http://payment.ucas.ac.cn/NetWorkUI/onlinePay'
+    print('[LOG] step 3, posting to', url)
+    response = spider.post(url, data=data)
+    spider.html_save(response, 'third_step.html')
+    return response
+# }}}
+def fouth_step():# {{{
+    url = 'http://payment.ucas.ac.cn/NetWorkUI/weixinPayAction?orderno=%s'%\
+            information['payOrderTrade']['orderno']
+
+    print('[LOG] step 4, getting from', url)
+    response = spider.get(url)
+    spider.html_save(response, 'fouth_step.html')
+    return response
+# }}}
+def fifth_step(urlcode):# {{{
+    data = {
+        'msgCode': 'SUCCESS',
+        'weixinMessage': '??',
+        'urlCode': urlcode,
+        'key': 'TkVUV09SS1BBWWtleQ==',
+    }
+    url = 'http://payment.ucas.ac.cn/NetWorkUI/weiXinQRCode?'
+    for key, value in data.items():
+        url = url + key + '=' + value + '&'
+    print('[LOG] step 5, getting from', url)
+    print('[OPT] please open the website shown above to pay the ticket! remembet to login before open.')
+    # response = spider.get(url)
+    # spider.html_save(response, 'fifth_step.html')
+    return response
+# }}}
+
+response = second_step()
+information = response.json()
+ret = information['returncode']
+print('[LOG] server return code:', ret)
+if ret == 'SUCCESS':
+    third_step()
+    response = fouth_step()
+    urlcode = re.compile('toUtf8(.*?);').findall(response.text)[0][2:-2]
+    fifth_step(urlcode)
