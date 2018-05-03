8a9182d1e7b4580f2d77f81bbe13830266777aa5
indestinee Chen <indestinee@gmail.com>
2018-05-03 20:21:23 +0800

    huge update

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from crawl.spider import *
-from crawl.utils import *
+from crawl2.spider import *
+from crawl2.utils import *
 import time, os
 import argparse
 
+user_save_file = 'data'
+
 def get_args():
     parser = argparse.ArgumentParser(description='UCAS_BUS')
-    parser.add_argument('-u', '--user', default='./data')
+    parser.add_argument('-u', '--user', default='default')
     args = parser.parse_args()
     return args
 
@@ -35,21 +37,21 @@ def first_step():# {{{
 # }}}
 def second_step():# {{{
     data = {
-        'routecode': '0015',            #   You need change
+        'routecode': cache.route['routecode'],  #   You need change
         'payAmt': '6.00',
-        'bookingdate': '2018-05-06',    #   You need change
+        'bookingdate': cache.date,    #   You need change
         'payProjectId': '4',        
-        'tel': user_cache.tel,
+        'tel': user_cache.load('tel'),
         'factorycode': 'R001',
     }
-    print(data)
+    print('[LOG] order information:', data)
     url = 'http://payment.ucas.ac.cn/NetWorkUI/reservedBusCreateOrder'
     print('[LOG] step 2, posting to', url)
     response = spider.post(url, data)
     spider.html_save(response, 'second_step.html')
     return response
 # }}}
-def third_step():# {{{
+def third_step(information):# {{{
     data = {
         'orderno': information['payOrderTrade']['orderno'],
         'orderamt': '6.00',
@@ -64,8 +66,8 @@ def third_step():# {{{
     spider.html_save(response, 'third_step.html')
     return response
 # }}}
-def fouth_step():# {{{
-    url = 'http://payment.ucas.ac.cn/NetWorkUI/weixinPayAction?orderno=%s'\
+def fouth_step(information):# {{{
+    url='http://payment.ucas.ac.cn/NetWorkUI/weixinPayAction?orderno=%s'\
             %information['payOrderTrade']['orderno']
     print('[LOG] step 4, getting from', url)
     response = spider.get(url)
@@ -87,10 +89,12 @@ def fifth_step(urlcode):# {{{
     print('[OPT] please open the website shown above to pay the ticket! remembet to login before open.')
     # response = spider.get(url)
     # spider.html_save(response, 'fifth_step.html')
-    return response
+    # return response
 # }}}
 
-user_file = os.path.join('data', args.user)
+if not os.path.isdir(user_save_file):
+    os.mkdir(user_save_file)
+user_file = os.path.join(user_save_file, args.user)
 
 while not spider.login(
         'http://payment.ucas.ac.cn/NetWorkUI/fontuserLogin', 
@@ -99,66 +103,97 @@ while not spider.login(
     ):
     print('[ERR] failed to login please check username, password and certcode')
 
-user_cache = Cache(user_filer)
+user_cache = Cache(user_file)
 
 cache.date = cache.load('date')
-date = input('[I N] date (yyyy-mm-dd) [default %s]: ' % cache.date)
-if date:
-    try:
-        t = date.replace('/', '-').split('-')
-        cache.date = '%04d-%02d-%02d'%(t[0], t[1], t[2])
-    except:
-        print('[ERR] Wrong format!!!')
+while True:
+    date = input('[I N] date (yyyy-mm-dd) [default %s]: ' % cache.date)
+    if date:
+        try:
+            t = date.replace('/', '-').split('-')
+            cache.date = '%04d-%02d-%02d'%\
+                    (int(t[0]), int(t[1]), int(t[2]))
+            cache.save(cache.date, 'date')
+            break
+        except:
+            print('[ERR] Wrong format!!!')
+    else:
+        if cache.date:
+            break
+print('[LOG] confirm date:', cache.date)
 
 cache.route = cache.load('route')
-print('[LOG] default route: {}'.format(cache.route))
-def route():
-    choice = input('[I N] new route? [y/n] (default n): ').tolower()
+print('[LOG] default route:')
+if cache.route:
+    print('  name: {}, time: {}, code: {}'.format(cache.route['routename'],\
+            cache.route['routetime'], cache.route['routecode']))
+else:
+    print('  none')
+
+def get_route():
+    choice = input('[I N] new route? [y/n] (default n): ').lower() \
+            if cache.route else 'y'
     if choice == 'n' or not choice:
         return cache.route
     elif choice == 'y':
-        try:
-            response = first_step.json()
-            print('[LOG] query return code', response['returncode'])
-            print('----    ----    choose route    ----    ----')
-            route_list = response['routelist']
-            for i, route in enumerate(route_list):
-                print('  [SEL] [{}] {}'.format(i, route))
-            choice = int(input('[I N] ID: '))
-            return route_list[choice]
-            print('----    ----    route selected    ----    ----')
-        except:
-            print('[ERR] getting route failed..')
-            return None
-
-while True:
-    route = route()
-    if route:
-        cache.route = route
-        cache.save(route, 'route')
-        break
-
-t = (3600 - int(time.time()) % 3600) + 90
-print('[LOG] sleep for %d secs' % t)
-print('[WRN] DO NOT login too early before system opens, there may be a time limit for COOCKIES!')
-time.sleep(t)
+        response = first_step().json()
+        print('[LOG] query return code', response['returncode'])
+        print('----    ----    choose route    ----    ----')
+        route_list = response['routelist']
+        for i, route in enumerate(route_list):
+            print('  [SEL] [{}] name: {}, time: {}, code: {}'.format(\
+                    i, route['routename'], route['routetime'],\
+                    route['routecode']))
+        choice = int(input('[I N] ID: '))
+        print('----    ----    route selected    ----    ----')
+        return route_list[choice]
 
 while True:
     try:
+        route = get_route()
+        if route:
+            cache.route = route
+            cache.save(route, 'route')
+            break
+    except:
+        print('[ERR] require route error..')
+
+print('[LOG] confirm route:')
+print('  name: {}, time: {}, code: {}'.format(cache.route['routename'],\
+        cache.route['routetime'], cache.route['routecode']))
+
+choice = input('[I N] sleep for the next new hour? [y/n] (default n): ').\
+        lower()
+
+def buy():
+    # try:
         response = second_step()
         information = response.json()
         ret = information['returncode']
         print('[LOG] server return code:', ret)
         if ret == 'SUCCESS':
-            third_step()
-            response = fouth_step()
+            third_step(information)
+            response = fouth_step(information)
             urlcode = re.compile('toUtf8(.*?);').\
                     findall(response.text)[0][2:-2]
             fifth_step(urlcode)
+            return True
         else:
             print('[LOG] full return', information)
-        break
-    except:
+            return False
+    # except:
         print('[ERR] failed to book, re-run.')
-        pass
+        return False
 
+if choice == 'y':
+    # print('[LOG] the following buying is a test..')
+    # buy()
+    print('[LOG] the real one is starting soon..')
+    t = (3600 - int(time.time()) % 3600) + 90
+    print('[LOG] sleep for %d secs' % t)
+    print('[WRN] DO NOT login too early before system opens, there may be a time limit for COOCKIES!')
+    time.sleep(t)
+
+        
+while not buy():
+    time.sleep(1)
